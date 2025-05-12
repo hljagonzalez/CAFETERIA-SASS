759401da54b0b27ac92717f73b991b78d38fae2d
Julio González <julio@MacBook-Air-de-Julio.local>
2025-05-12 19:03:42 +0200

    3.- Tratamiento de imagenes y comenzando con el Header

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,35 @@ const sass = require ('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss')
 const autoprefixer  = require('autoprefixer')
 
+const imagemin = require("gulp-imagemin");
+
+const webp = require("gulp-webp");
+
+const avif = require("gulp-avif");
+
+function versionAvif() {
+ return src("src/img/**/*.{png,jpg}")
+   .pipe(avif())
+   .pipe(dest("build/img"));
+}
+
+function versionWebp() {
+ const opciones = {
+   quality: 50 //quality es la calidad de la imagen, 50 es un valor intermedio.
+ }
+ return src("src/img/**/*.{png,jpg}") //sólo quiero convertir a webp las imágenes png y jpg
+   .pipe(webp(opciones))
+   .pipe(dest("build/img"));
+}
+
+//Si no quiero usar el done cómo parámetro, puedo hacerlo así usando return, pero no puedo usar done y return a la vez.
+function imagenes() {
+ return src("src/img/**/*")
+  // IMPORTANTE. LA TAREA DE OPTIMIZACIÓN ANTES DE LA TAREA DE COPIAR LA IMAGEN EN BUILD.
+   .pipe(imagemin({optimizationLevel: 3})) 
+   //imagemin es un plugin que optimiza las imágenes, optimizationLevel: 3 es el nivel de optimización, 3 es el máximo.
+   .pipe(dest("build/img"));
+}
 
 function css(done) {
  //compilar sass
@@ -16,10 +45,15 @@ function css(done) {
 }
 function dev(){
  watch('src/scss/**/*.scss',css) // atento a cambios en cualquier archivo .scss incluido en cualquier carpeta dentro de la carpeta 'src/scss'
+ watch("src/img/**/*", imagenes);
 }
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css,dev); // Mejor series, para que compile primero, y luego se quede escuchando. La tarea con los
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev); // Mejor series, para que compile primero, y luego se quede escuchando. La tarea con los
 //watch siempre la última
 
 //series -  lanza las tareas una detrás de otra, hasta que no termina una no empieza la otra
